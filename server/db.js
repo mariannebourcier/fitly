b226f803582f258f9b485051e65b579ad0833d4d
mariannebourcier <marianne.bourcier@gmail.com>
2022-11-03 12:10:13 -0600

    Change database table row, setup db connection

--- server/db.js
+++ server/db.js
@@ -4,8 +4,10 @@ let dbParams = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST
 }
 
 const pool = new Pool(dbParams);
 
+module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 8080;
+const pool = require("./db");
 
 //middleware
 app.use(cors());
@@ -9,4 +10,11 @@ app.use(express.json()); //access req body
 
 app.listen(port, () => {
   console.log(`Server is listening on ${port}`);
-});
+}); 
+
+//Routes
+//home
+app.get()
+//get all users
+app.get()
+
