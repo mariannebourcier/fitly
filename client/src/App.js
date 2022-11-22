1c895c760232eeb665307d9441c20e6f7576d808
mariannebourcier <marianne.bourcier@gmail.com>
2022-11-22 14:38:03 -0700

    Router and routes setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
 
+//top level component
+//manage all routes
 function App() {
   return (
     <div className="App">
      <header>
        Fitly
      </header>
+     <Routes> 
+       <Route path="/" element={<Home />} />
+
+     </Routes>
     </div>
   );
 }
 
 export default App;
+//path to match
+//element : what to render 
