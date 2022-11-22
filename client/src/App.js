32017fabfa4e9c4444b6f1e8f2a3e78a33d36141
mariannebourcier <marianne.bourcier@gmail.com>
2022-11-22 15:09:01 -0700

    Add navigation with links to pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Profile from "./components/Profile/Profile";
 import Signup from "./components/Signup/Signup";
@@ -12,13 +12,24 @@ function App() {
       <header>
         Fitly
       </header>
+      <nav>
+      <Link to="/profile">Profile</Link>
+      <Link to="/signup">Sign Up</Link>
+      <Link to="/signin">Sign In</Link>
+      <Link to="/calculate">Calculate</Link>
+
+      </nav>
       <Routes>
         <Route path="/" element={<Home title="Welcome to Fitly" />} />
         <Route path="profile" element={<Profile title="This is the profile page!" />} />
         <Route path="signup" element={<Signup title="This is the signup page!" />} />
         <Route path="signin" element={<Signin title="This is the signin page!" />} />
-
+        <Route path="calculate" element={<h1>Calculate your macros</h1>} />
       </Routes>
+
+      <footer>
+        Thanks for visiting Fitly!
+      </footer>
     </div>
   );
 }
