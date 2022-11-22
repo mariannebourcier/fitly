bf12ee5d7c30da6e38dc92a7c5bb146e2554e2dd
mariannebourcier <marianne.bourcier@gmail.com>
2022-11-22 14:48:14 -0700

    Add route for profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Profile from "./components/Profile/Profile"
 
 //top level component
 //manage all routes
 function App() {
   return (
     <div className="App">
-     <header>
-       Fitly
-     </header>
-     <Routes> 
-       <Route path="/" element={<Home />} />
-
-     </Routes>
+      <header>
+        Fitly
+      </header>
+      <Routes>
+        <Route path="/" element={<Home title="Welcome to Fitly" />} />
+        <Route path="/profile" element={<Profile title="This is the profile page!" />} />
+      </Routes>
     </div>
   );
 }
@@ -21,3 +22,4 @@ function App() {
 export default App;
 //path to match
 //element : what to render 
+//pass props in element tag, destructure in file and use 
